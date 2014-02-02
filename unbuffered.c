2a9fbdb01637adae98715e974726f991e6f00298
Valentin Hilbig <webmaster@scylla-charybdis.com>
2014-02-02 20:56:13 +0100

    VERSION 0.5.0 - Option -d to debug forked producer (catch stderr as well) - Option -v to verbosely show the forked producer output

--- unbuffered.c
+++ unbuffered.c
@@ -1,8 +1,7 @@
-/* $Header$
- *
+/*
  * Copy stdin to stdout and stderr, unbuffered.
  *
- * Copyright (C)2006-2011 by Valentin Hilbig
+ * Copyright (C)2006-2013 by Valentin Hilbig
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,49 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * $Log$
- * Revision 1.16  2011-10-22 22:14:21  tino
- * OOPS, added the flag as wrong variable type
- *
- * Revision 1.15  2011-10-22 22:12:37  tino
- * Option -b as bugfix for -c
- *
- * Revision 1.14  2011-10-22 20:40:18  tino
- * Options -i and -o
- *
- * Revision 1.13  2011-07-29 20:36:29  tino
- * Option -a fixed
- *
- * Revision 1.12  2011-02-13 23:11:22  tino
- * New version which can fork a producer
- *
- * Revision 1.11  2011-02-13 23:05:27  tino
- * compile fixes
- *
- * Revision 1.10  2011-02-11 21:10:49  tino
- * Typos fixed
- *
- * Revision 1.9  2011-02-11 21:06:57  tino
- * better Usage
- *
- * Revision 1.8  2009-07-06 00:28:38  tino
- * Better cat mode
- *
- * Revision 1.7  2009-05-23 17:12:53  tino
- * Option -a and -n changes
- *
- * Revision 1.6  2009-05-23 10:22:39  tino
- * Options -l and -u
- *
- * Revision 1.5  2009-03-17 10:41:08  tino
- * Unready next version
- *
- * Revision 1.4  2008-10-17 19:17:54  tino
- * Usage corrected
- *
- * Revision 1.2  2008-07-08 20:10:03  tino
- * Bugfix (hexdump bytecount) and Option -c
  */
 
 #include "tino/main_getext.h"
@@ -74,7 +30,7 @@
 #undef HAVE_ESCAPE_XML
 #undef HAVE_FIELD_FORMAT
 
-static int	flag_linecount, flag_hexdump;
+static int	flag_linecount, flag_hexdump, flag_both, flag_verbose;
 #ifdef HAVE_ESCAPE_XML
 static int	flag_escape;
 #endif
@@ -88,7 +44,9 @@ static int	in_line, line_nr, flag_cat, flag_utc, flag_localtime, flag_micro, fla
 static int	fd_in, fd_out;
 static unsigned long long	byte_pos;
 static TINO_DATA		out;
+static int			out_is_open;
 static TINO_BUF			prefix;
+static pid_t			producer;
 
 /* This is plain crap, really, however there is no good support for
  * this all in my lib yet (there is no support for such things in
@@ -126,6 +84,36 @@ add_time(struct tm *fn(const time_t *timep))
     add_prefix("[%04d%02d%02d-%02d%02d%02d]", 1900+tm->tm_year, 1+tm->tm_mon, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
 }
 
+static void
+start_line(int verbose)
+{
+  char	flg;
+
+  if (flag_localtime || flag_utc)
+    gettimeofday(&stamp, NULL);
+
+  tino_buf_resetO(&prefix);
+
+  add_prefix("");
+  if (flag_localtime)
+    add_time(localtime);
+  if (flag_utc)
+    add_time(gmtime);
+
+  flg	= verbose ? '-' : ' ';
+  if (flag_verbose && producer)
+    add_prefix("[%ld]", (long)producer);
+
+  if (flag_linecount)
+    {
+      if (!in_line)
+	line_nr++;
+      add_prefix((flag_linecount==1 ? "%5d%c" : flag_linecount==2 ? "%05d%c" : "%d%c"), line_nr, flg);
+    }
+  else if (flag_verbose)
+    add_prefix("%c", flg);
+}
+
 /* This is plain crap, really, however there is no good support for
  * this all in my lib yet (there is no support for such things in
  * nearly no language, either.  Python is best in this regard).
@@ -135,27 +123,9 @@ dump_line(const char *ptr, size_t n, int lineend)
 {
   const char		*p;
 
-  if (flag_localtime || flag_utc)
-    gettimeofday(&stamp, NULL);
-  tino_buf_resetO(&prefix);
-  if (flag_localtime)
-    add_time(localtime);
-  if (flag_utc)
-    add_time(gmtime);
-  if (flag_linecount)
-    {
-      if (!in_line)
-	line_nr++;
-      add_prefix((flag_linecount==1 ? "%5d " : flag_linecount==2 ? "%05d " : "%d "), line_nr);
-    }
+  start_line(0);
+  p = tino_buf_get_sN(&prefix);
 
-  /* PHP parses A ? B : C ? E : F as (A ? B : C) ? E : F which is
-   * plain crap.  Luckily we are in C which parses this correctly like
-   * if-else, which is A ? B : ( C ?  D : E )
-   */
-  p	= tino_buf_get_lenO(&prefix) ? tino_buf_get_sN(&prefix) : in_line ? line_cont_prefix : line_prefix;
-  if (!p)
-    p	= "";
   if (flag_hexdump)
     tino_xd(&out, p, 8, byte_pos, (const unsigned char *)ptr, n+lineend);
   else
@@ -197,13 +167,49 @@ terminate()
 #endif
 
 static void
+out_open(void)
+{
+  if (out_is_open)
+    return;
+  if (!append_file)
+    {
+      tino_data_fileA(&out, fd_out>=0 ? fd_out : flag_cat ? 1 : 2);
+      out_is_open = -1;
+    }
+  else
+    {
+      tino_data_fileA(&out, tino_file_open_createE(append_file, O_APPEND|O_WRONLY, 0666));
+      out_is_open	= 1;
+    }
+}
+
+
+static void
+out_verbose()
+{
+}
+
+static void
+out_close(void)
+{
+  if (!out_is_open)
+    return;
+  tino_data_freeA(&out);
+  out_is_open	= 0;
+}
+
+static void
+out_flush(void)
+{
+  if (out_is_open>0)
+    out_close();
+}
+
+static void
 unbuffered(const char *arg0, int argc, char **argv)
 {
   TINO_BUF	buf;
-  int		is_open;
   int		fds[2];
-  pid_t		producer;
-
 
   producer = 0;
   if (argc)
@@ -213,7 +219,7 @@ unbuffered(const char *arg0, int argc, char **argv)
 
       /* catch the child's output for preprocessing
        */
-      producer = tino_fork_exec(0,fds[1],2, argv, NULL, 0, NULL);
+      producer = tino_fork_exec(0, fds[1], (flag_both ? fds[1] : 2), argv, NULL, 0, NULL);
       tino_file_closeE(fds[1]);
 
       /* Move the pipe's output to our stdin
@@ -230,19 +236,24 @@ unbuffered(const char *arg0, int argc, char **argv)
       terminate();	/* catch early terminated childs	*/
 #endif
     }
-  is_open	= 0;
-  if (!append_file)
-    tino_data_fileA(&out, fd_out>=0 ? fd_out : flag_cat ? 1 : 2);
+
   tino_buf_initO(&buf);
+  if (producer && flag_verbose)
+    {
+      start_line(1);
+      add_prefix("start");
+      for (; *argv; argv++)
+	add_prefix(" '%s'", *argv);	/* XXX TODO sadly, escape is implemented in tino_io, not in tino_buf	*/
+      add_prefix("\n");
+      out_open();
+      tino_data_putsA(&out, tino_buf_get_sN(&prefix));
+    }
+
   while (tino_buf_readE(&buf, fd_in, -1))
     {
       size_t		n;
 
-      if (append_file && !is_open)
-	{
-	  tino_data_fileA(&out, tino_file_open_createE(append_file, O_APPEND|O_WRONLY, 0666));
-	  is_open	= 1;
-	}
+      out_open();
       while ((n=tino_buf_get_lenO(&buf))>0)
 	{
 	  const char	*ptr;
@@ -256,7 +267,7 @@ unbuffered(const char *arg0, int argc, char **argv)
 		dump_line(ptr+p, k-p, 1);
 		p	= k+1;
 	      }
-
+	  /* k=n	*/
 	  if (p && flag_buffer)
 	    {
 	      /* Fix: If we are catting, do not output incomplete
@@ -274,23 +285,35 @@ unbuffered(const char *arg0, int argc, char **argv)
 	  if (p<n)
 	    dump_line(ptr+p, n-p, 0);
 	  if (flag_cat)
-	    tino_buf_advanceO(&buf, k);
-	  else if (tino_buf_write_away_allE(&buf, 1, k))
+	    tino_buf_advanceO(&buf, n);
+	  else if (tino_buf_write_away_allE(&buf, 1, n))
 	    {
 	      /* silently drop out	*/
 	      *tino_main_errflag	= 1;
 	      break;
 	    }
 	}
-      if (is_open)
-	{
-	  tino_data_freeA(&out);
-	  is_open	= 0;
-	}
+      out_flush();
     }
-  tino_data_freeA(&out);	/* close(2)	*/
+  {
+      size_t		n;
+
+      /* in case of flag_buffer: send the rest	*/
+      if ((n=tino_buf_get_lenO(&buf))>0)
+	{
+	  const char	*ptr;
+
+	  out_open();
+	  ptr	= tino_buf_getN(&buf);
+	  dump_line(ptr, n, 0);
+	  if (!flag_cat && tino_buf_write_away_allE(&buf, 1, n))
+	    *tino_main_errflag	= 1;
+	}
+  }
   if (producer)
     {
+      char *cause;
+
 #if 0
       tino_sigdummy(SIGCHLD);	/* prevent reentrance of waitpid()	*/
 #endif
@@ -298,8 +321,16 @@ unbuffered(const char *arg0, int argc, char **argv)
        * so give the child the chance to terminate.
        */
       tino_file_closeE(0);
-      *tino_main_errflag	= tino_wait_child_exact(producer, NULL);
+      *tino_main_errflag	= tino_wait_child_exact(producer, &cause);
+      if (flag_verbose)
+	{
+	  start_line(1);
+	  add_prefix("end %s\n", cause);
+	  out_open();
+	  tino_data_putsA(&out, tino_buf_get_sN(&prefix));
+	}
     }
+  out_close();			/* close(2)	*/
 }
 
 int
@@ -336,7 +367,7 @@ main(int argc, char **argv)
 		      TINO_GETOPT_FLAG
 		      "b	Buffer partial lines\n"
 		      "		When incomplete lines are read from stdin, buffer them.\n"
-		      "		This way only full lines are output, always.  See also -u"
+		      "		Except the last line you only see full lines.  See also -u"
 		      , &flag_buffer,
 
 		      TINO_GETOPT_FLAG
@@ -344,6 +375,11 @@ main(int argc, char **argv)
 		      "		producer | unbuffered 2>&1 >/dev/null | consumer\n"
 		      "		Without this option input is copied to output unchanged"
 		      , &flag_cat,
+
+		      TINO_GETOPT_FLAG
+		      "d	Debug forked producer by redirecting STDERR to STDOUT.\n"
+		      "		Makes only sense if not reading stdin from a pipe."
+		      , &flag_both,
 #ifdef HAVE_ESCAPE_XML
 		      TINO_GETOPT_FLAG
 		      TINO_GETOPT_MAX
@@ -363,6 +399,7 @@ main(int argc, char **argv)
 		      , &field_format,
 		      , "p,[l],[u],n5 ,cs"
 #endif
+		      /* g */
 		      TINO_GETOPT_INT
 		      TINO_GETOPT_DEFAULT
 		      TINO_GETOPT_MIN
@@ -371,6 +408,7 @@ main(int argc, char **argv)
 		      0,
 		      0,
 
+		      /* jk */
 		      TINO_GETOPT_FLAG
 		      "l	Local timestamp each line"
 		      , &flag_localtime,
@@ -422,9 +460,16 @@ main(int argc, char **argv)
 		      , &flag_utc,
 
 		      TINO_GETOPT_FLAG
+		      "v	Verbose report return code of forked producer.\n"
+		      "		Also adds a space after the prefixes for normal lines."
+		      , &flag_verbose,
+
+		      /* w */
+		      TINO_GETOPT_FLAG
 		      "x	heXdump output"
 		      , &flag_hexdump,
 
+		      /* yz */
 		      NULL
 		      );
 }
